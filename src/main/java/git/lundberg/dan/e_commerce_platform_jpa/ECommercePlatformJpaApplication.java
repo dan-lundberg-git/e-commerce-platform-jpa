d6122e6f5ce577ffec92ffd6f21d696a65aae2b9
Dan Lundberg <dan-lundberg@outlook.com>
2026-09-09 10:10:31 +0200

    Initial commit. Spring Initializr and Lumo AI used for scaffolding and application.properties. Swapped MySQL for PostgreSQL instead.

---
+++ src/main/java/git/lundberg/dan/e_commerce_platform_jpa/ECommercePlatformJpaApplication.java
@@ -0,0 +1,13 @@
+package git.lundberg.dan.e_commerce_platform_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePlatformJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommercePlatformJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/lundberg/dan/e_commerce_platform_jpa/ECommercePlatformJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package git.lundberg.dan.e_commerce_platform_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercePlatformJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
