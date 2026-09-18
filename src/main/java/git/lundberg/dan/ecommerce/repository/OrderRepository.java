d04b77bf6a553796a3c3fbdd8d7af480d0aff0dd
Dan Lundberg <dan-lundberg@outlook.com>
2026-09-18 10:31:40 +0200

    Add derived queries for OrderRepository. Using @EntityGraph to solve the N+1 issue for now. See comments though.

--- src/main/java/git/lundberg/dan/ecommerce/repository/OrderRepository.java
+++ src/main/java/git/lundberg/dan/ecommerce/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package git.lundberg.dan.ecommerce.repository;
 
 import git.lundberg.dan.ecommerce.entity.Order;
+import git.lundberg.dan.ecommerce.utils.OrderStatus;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     // TODO:
@@ -18,4 +22,20 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     //      For the “find by status” query, use one of the following approaches:
     //      - @EntityGraph to load items in the same query, or
     //      - JOIN FETCH in JPQL.
+
+    List<Order> findByCustomer_Id(Long customerId);
+
+    @EntityGraph(attributePaths = "orderItems")
+    List<Order> findDistinctByOrderStatus(OrderStatus orderStatus);
+
+    // What's really happening with @EntityGraph above:
+    // SELECT o.*, oi.*
+    // FROM orders o
+    // LEFT JOIN order_items oi ON oi.order_id = o.id
+    // WHERE o.status = ?
+
+    // Not sure if I want to use @EntityGraph, since there's too much "magic" happening.
+    // A direct approach with full control would be like below, and probably more to my liking.
+    // @Query("SELECT DISTINCT o FROM Order o LEFT JOIN FETCH o.orderItems WHERE o.orderStatus = :status")
+    // List<Order> findByOrderStatusWithItems(@Param("status") OrderStatus status);
 }
